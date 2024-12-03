896be6a4c627bf20278036f5faa22c5c944e7c67
Arthur Pastel <arthur.pastel@gmail.com>
2024-12-03 16:23:48 +0100

    feat: add benches

--- newday.sh
+++ newday.sh
@@ -4,12 +4,8 @@ DAY=$(echo $1 | sed 's/^0*//')
 
 set -e
 
-source .env
-
 mkdir -p inputs puzzles
 aoc download -d $DAY --input-file inputs/day$DAY.txt --puzzle-file puzzles/day$DAY.md --overwrite
 cp -n template.rs src/day$DAY.rs
-
-code puzzles/day$DAY.md src/bin/day$DAY.rs inputs/day$DAY.txt
 
 echo pub mod day$DAY\; >>src/lib.rs
