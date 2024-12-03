9759c2d627fa562f14df3932903cb4632dfd342f
Arthur Pastel <arthur.pastel@gmail.com>
2024-12-03 22:54:18 +0100

    feat: simple (stupid) day3

---
+++ src/day3.rs
@@ -0,0 +1,76 @@
+use regex::Regex;
+use std::sync::LazyLock;
+
+static R_1: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap());
+
+pub fn part1(input: &str) -> u64 {
+    let mut sum = 0;
+    for caps in R_1.captures_iter(input) {
+        let a = caps[1].parse::<u64>().unwrap();
+        let b = caps[2].parse::<u64>().unwrap();
+        let result = a * b;
+        sum += result;
+    }
+    sum
+}
+
+pub fn part2(input: &str) -> u64 {
+    let mut sum = 0;
+    let mut start = 0;
+    let mut end;
+    loop {
+        end = input[start..]
+            .find("don't()")
+            .map(|i| i + start)
+            .unwrap_or(input.len());
+
+        sum += part1(&input[start..end]);
+
+        if end >= input.len() {
+            break;
+        }
+        start = input[end..]
+            .find("do()")
+            .map(|i| i + end)
+            .unwrap_or(input.len());
+
+        if start >= input.len() {
+            break;
+        }
+    }
+    sum
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = crate::utils::sample_input! {"
+        xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+        "};
+        let expected = 161;
+        assert_eq!(part1(&input), expected);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = crate::utils::sample_input! {"
+        xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+        "};
+        let expected = 48;
+        assert_eq!(part2(&input), expected);
+    }
+
+    #[test]
+    fn run_part1() {
+        let input = crate::utils::get_day_input!();
+        println!("Part 1: {}", part1(&input));
+    }
+    #[test]
+    fn run_part2() {
+        let input = crate::utils::get_day_input!();
+        println!("Part 2: {}", part2(&input));
+    }
+}
