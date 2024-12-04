9b5b1cd797967aaa5cbc2e66dc4422ec638e592c
Arthur Pastel <arthur.pastel@gmail.com>
2024-12-04 23:48:28 +0100

    chore: improve template

--- newday.sh
+++ newday.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # Usage ./newday.sh <DAY>
-DAY=$(echo $1 | sed 's/^0*//')
 
 set -e
 
+DAY=$(echo $1 | sed 's/^0*//') # Remove leading zeros
+
+# Create the input and puzzle files
 mkdir -p inputs puzzles
 aoc download -d $DAY --input-file inputs/day$DAY.txt --puzzle-file puzzles/day$DAY.md --overwrite
-cp -n template.rs src/day$DAY.rs
 
+# Create the day file from the template
+cp -n "src/day_template.rs" src/day$DAY.rs
+
+# Add the day as a module to the lib
 echo pub mod day$DAY\; >>src/lib.rs
+
+# Add the day to the benches template
 sed -i "s/\(benches!(.*\));/\1, $DAY);/" ./benches/bench_days.rs
--- src/day_template.rs
+++ src/day_template.rs
@@ -11,29 +11,34 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_part1() {
+    fn test_part1_0() {
         let input = crate::utils::sample_input! {"
         "};
-        let expected = 11;
+        let expected = 0;
         assert_eq!(part1(&input), expected);
-    }
-
-    #[test]
-    fn test_part2() {
-        let input = crate::utils::sample_input! {"
-        "};
-        let expected = 31;
-        assert_eq!(part2(&input), expected);
     }
 
     #[test]
     fn run_part1() {
         let input = crate::utils::get_day_input!();
-        println!("Part 1: {}", part1(&input));
+        let output = part1(&input);
+        println!("Part 1: {}", output);
+        assert_eq!(output, 0);
     }
+
+    #[test]
+    fn test_part2_0() {
+        let input = crate::utils::sample_input! {"
+        "};
+        let expected = 0;
+        assert_eq!(part2(&input), expected);
+    }
+
     #[test]
     fn run_part2() {
         let input = crate::utils::get_day_input!();
-        println!("Part 2: {}", part2(&input));
+        let output = part2(&input);
+        println!("Part 2: {}", output);
+        assert_eq!(output, 0);
     }
 }
